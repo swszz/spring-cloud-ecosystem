8dee8b909f5b0b0a5f726ad89397c94a029c4154
swszz <swsz@kakao.com>
2024-06-23 15:48:09 +0900

    chore: 프로젝트 초기화

--- training_for_spring_cloud_features/bom/trace/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-tracing-bridge-otel")
-    implementation("io.opentelemetry:opentelemetry-exporter-zipkin")
-}
