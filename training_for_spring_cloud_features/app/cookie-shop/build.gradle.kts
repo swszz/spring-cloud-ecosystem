98c6c23afca91ec363e9b76c8e498dc3c2ce1641
swszz <swsz@kakao.com>
2024-06-25 23:53:32 +0900

    feat: configuration server

--- training_for_spring_cloud_features/app/cookie-shop/build.gradle.kts
+++ training_for_spring_cloud_features/app/cookie-shop/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("de.codecentric:spring-boot-admin-starter-client:3.3.0")
     implementation(project(":training_for_spring_cloud_features:lib:trace"))
 }
