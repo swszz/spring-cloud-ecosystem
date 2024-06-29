44dbcb22f035c9c4996ba851777675a93ccfbd8e
swszz <swsz@kakao.com>
2024-06-29 19:38:09 +0900

    fix: zipkin tracing not working on spring cloud stream

--- training_for_spring_cloud_features/app/cookie-shop/src/main/kotlin/com/swzz/sce/app/cookieshop/CookieController.kt
+++ training_for_spring_cloud_features/app/cookie-shop/src/main/kotlin/com/swzz/sce/app/cookieshop/CookieController.kt
@@ -1,9 +1,12 @@
 package com.swzz.sce.app.cookieshop
 
+import org.springframework.boot.context.event.ApplicationStartedEvent
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.stream.function.StreamBridge
+import org.springframework.context.event.EventListener
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Hooks
 import java.util.*
 
 
@@ -13,6 +16,11 @@ class CookieController(
     private val properties: CookieProperties,
     private val streamBridge: StreamBridge,
 ) {
+    @EventListener(ApplicationStartedEvent::class)
+    fun setup() {
+        Hooks.enableAutomaticContextPropagation()
+    }
+
     @GetMapping("/cookies")
     suspend fun getCookies() = properties.names
 
